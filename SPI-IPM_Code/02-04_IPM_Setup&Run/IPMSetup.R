42b42d61ad3cc8df3b1ef776cb977c42d80be8ee
ChloeRNater <chloe.r.nater@ntnu.no>
2021-09-30 09:45:29 +0200

    Added Chapter 2 content to manual
    Population model description, introduction to data likelihoods

--- SPI-IPM_Code/02-04_IPM_Setup&Run/IPMSetup.R
+++ SPI-IPM_Code/02-04_IPM_Setup&Run/IPMSetup.R
@@ -51,10 +51,10 @@ str(PFC.data)
 # NoLaid[y] = Number of eggs laid in successful brood y
 # NoF_year[y] = Year of successful brood y
 # NoF_FAge[y] = Age class of the mother of successful brood y (1 = yearling, 2 = adult)
- 
+
 # ImmNoObs[t] = Total number of immigrants (= unmarked adults) captured in year t
-# ImmAgeProps[a] = All-time average proportion of known age immigrants in age class a (1 = yearling, 2 = adult) 
-# ImmAgeProps_t[a,t] = Proportion of known age immigrants in age class a (1 = yearling, 2 = adult) in year t 
+# ImmAgeProps[a] = All-time average proportion of known age immigrants in age class a (1 = yearling, 2 = adult)
+# ImmAgeProps_t[a,t] = Proportion of known age immigrants in age class a (1 = yearling, 2 = adult) in year t
 
 # NS_Data[t] = Whether or not any nest data was collected in year t
 # PropCapBrood[t] = the proportion of nests for which the female was captured in year t
@@ -74,20 +74,20 @@ env.data <- eval(parse(text = paste0('EnvData$', PopID, '$WindowY')))
 
 ## Arrange constants
 PFC.IPMconstants <- list(Tmax = length(PFC.data$NestCount), A = 2,
-						             ageclass.sum = PFC.data$ageclass.sum, 
-						             first.sum = PFC.data$first.sum, 
+						             ageclass.sum = PFC.data$ageclass.sum,
+						             first.sum = PFC.data$first.sum,
 						             last.sum = PFC.data$last.sum,
 						             n.CH = dim(PFC.data$y.sum)[1],
-                    	   EggNoSP = PFC.data$EggNoSP, 
-                    	   CS_year = PFC.data$CS_year, 
-						             CS_FAge = PFC.data$CS_FAge, 
+                    	   EggNoSP = PFC.data$EggNoSP,
+                    	   CS_year = PFC.data$CS_year,
+						             CS_FAge = PFC.data$CS_FAge,
                     	   CS_X = length(PFC.data$ClutchSize),
                     	   FledgedSP = PFC.data$FledgedSP,
-                    	   F_year = PFC.data$F_year, 
-						             F_FAge = PFC.data$F_FAge, 
+                    	   F_year = PFC.data$F_year,
+						             F_FAge = PFC.data$F_FAge,
                     	   F_X = length(PFC.data$Fledged),
-						             NoF_year = PFC.data$NoF_year, 
-						             NoF_FAge = PFC.data$NoF_FAge, 
+						             NoF_year = PFC.data$NoF_year,
+						             NoF_FAge = PFC.data$NoF_FAge,
 						             NoF_X = length(PFC.data$NoFledged),
                     	   #ImmAgeProp = PFC.data$ImmAgeProp,
 						             NS_Data = PFC.data$NS_Data,
@@ -96,14 +96,14 @@ PFC.IPMconstants <- list(Tmax = length(PFC.data$NestCount), A = 2,
 						             AvgImm.limit = round(max(PFC.data$ImmNoObs)*5))
 
 ## Arrange data
-PFC.IPMdata <- list(y.sum = PFC.data$y.sum, 
+PFC.IPMdata <- list(y.sum = PFC.data$y.sum,
                     CHs.count = PFC.data$CHs.count,
 					          PropCapBrood = c(PFC.data$PropCapBrood, 1), # Adding 1 for Tmax+1 (required in model, but does not affect estimation)
 					          NestCount = PFC.data$NestCount,
                     EggNoTot = PFC.data$EggNoTot,
                     ClutchSize = PFC.data$ClutchSize,
                     FledgedTot = PFC.data$FledgedTot,
-                    anyFledged = PFC.data$anyFledged, 
+                    anyFledged = PFC.data$anyFledged,
 					          NoFledged = PFC.data$NoFledged,
 					          NoLaid = PFC.data$NoLaid,
                     ImmNoObs = PFC.data$ImmNoObs,
@@ -124,7 +124,7 @@ dCJS_vv_sum <- nimbleFunction(
   run = function(x = double(1),    ## standard name for the "data"
                  probSurvive = double(1),
                  probCapture = double(1),
-                 mult = double(0), #! NEWLY ADDED: argument stating number of occurences of same capture history in entire dataset 
+                 mult = double(0), #! NEWLY ADDED: argument stating number of occurences of same capture history in entire dataset
                  len = double(0, default = 0),
                  log = integer(0, default = 0) ## required log argument
   ) {
@@ -135,7 +135,7 @@ dCJS_vv_sum <- nimbleFunction(
       stop("Length of probSurvive must be at least length of data minus 1.")
     if (length(x) != length(probCapture)) stop("Length of probCapture does not match length of data.")
     if (x[1] != 1) stop("dCJS requires specifying first capture. x[1] must equal 1.")
-    
+
     ## Note the calculations used here are actually in hidden Markov model form.
     probAliveGivenHistory <- 1
     ## logProbData will be the final answer
@@ -214,133 +214,133 @@ registerDistributions(list(
 ####################
 
 PFC.IPMcode <- nimbleCode({
-	
+
 	#******************#
 	# POPULATION MODEL #
 	#******************#
-	
+
 	## Parameters:
-	
+
 	## Data & constants:
-	
-	
+
+
 	#---------------#
 	# Process model #
 	#---------------#
-	
+
  	for (t in 1:Tmax){
- 		
- 		## 1) Breeding decision
  		for(a in 1:A){
+
+ 		  ## 1) Breeding decision
  			B[a,t] ~ dbin(pB[a,t], N[a,t])
- 			
- 		## 2) Offspring production
- 		Juv[a,t] ~ dpois(B[a,t]*CS[a,t]*pNS[t]*sN[a,t]*0.5)
- 		
+
+ 		  ## 2) Offspring production
+ 		  Juv[a,t] ~ dpois(B[a,t]*CS[a,t]*pNS[t]*sN[a,t]*0.5)
+
  		}
- 	}	
- 		
- 		## 3) Annual survival of local birds
-		
+ 	}
+
  	  for (t in 1:(Tmax-1)){
+
+ 	    ## 3) Annual survival of local birds
 		  # Juveniles -> Yearlings
 		  localN[1,t+1] ~ dbin(sJ[t], sum(Juv[1:A,t]))
-		
+
 		  # Yearlings/Adults -> adults
  		  localN[2,t+1] ~ dbin(sA[t], sum(N[1:A,t]))
- 		
+
  		  ## 4) Immigration
  		  for(a in 1:A){
  			  N[a,t+1] <- localN[a,t+1] + Imm[a,t+1]
  		  }
- 		
+
  		  ## Sum population size
  		  Ntot[t] <- sum(N[1:A,t])
  		  Btot[t] <- sum(B[1:A,t])
- 	  }	
-    
+ 	  }
+
     Ntot[Tmax] <- sum(N[1:A,Tmax])
-  
+
     #----------------------#
     # Priors & constraints #
     #----------------------#
-    
+
      ## Initial population sizes
 
      for(a in 1:A){
      	localN[a,1] ~ dcat(DU.prior[1:N1.limit])
       N[a,1] <- localN[a,1] + Imm[a,1]
      }
-     
+
      DU.prior[1:N1.limit] <- 1/N1.limit
-     
-	# NOTES: 
+
+	# NOTES:
 	# The above formulation assumes a discrete uniform prior for initial population size,
 	# which requires an upper boundary (N1.limit) to be provided as data
 	# More informative priors (e.g. rounded normal with data-informed mean) could be considered.
 
-	
+
 	#**********************************#
 	# BREEDING POPULATION COUNT MODULE #
 	#**********************************#
-	
+
 	## Parameters:
-	
+
 	## Data & constants:
 
-	
+
 	#------------#
 	# Likelihood #
 	#------------#
-	
- 	for(t in 1:Tmax){ 		
+
+ 	for(t in 1:Tmax){
      	NestCount[t] ~ dpois(sum(B[1:A,t])*NS_Data[t])
   }
-	
-	# NOTES: 
+
+	# NOTES:
 	# Alternatively, one could use a truncated normal distribution here instead of the Poisson
-	
+
 	#------------------------#
 	# Priors and constraints #
 	#------------------------#
-	
+
 
   #********************#
 	# IMMIGRATION MODULE #
 	#********************#
-	
+
 	## Parameters:
-	
+
 	## Data and constants:
 
-	
+
      #------------#
      # Likelihood #
      #------------#
-     
+
      ## Likelihood for the number/age distribution of immigrant females
      for (t in 2:Tmax){
-       
+
        # Latent true number of breeding immigrants (count observation model)
        ImmNoObs[t] ~ dpois(sum(ImmB[1:A,t])*PropImmDetect[t])
-       
+
        # Number of breeding immigrants per age class
        for(a in 1:A){
          ImmB[a,t] ~ dbin(pB[a,t], Imm[a,t])
        }
-     } 
-     
+     }
+
      ImmNoObs[1] <- 0
      ImmB[1:A,1] <- 0
-     
-     
+
+
      #------------------------#
      # Priors and constraints #
      #------------------------#
-     
+
      ## Latent number of all immigrants (breeding & non-breeding) per age class
      Imm[1:A,1] <- 0
-     
+
      for(t in 2:Tmax){
        for(a in 1:A){
 
@@ -348,29 +348,29 @@ PFC.IPMcode <- nimbleCode({
          Imm[a,t] <- round(ImmCont[a,t])
        }
      }
-     
+
     for(a in 1:A){
       AvgImm[a] ~ dunif(0, AvgImm.limit)
       sigma.Imm[a] ~ dunif(0, AvgImm.limit*10)
     }
-     
+
      ## Immigrant detection (= marking) probability
      for(t in 1:Tmax){
        PropImmDetect[t] ~ dunif(0, 1)
      }
-	
+
 	#***********************#
 	# MARK-RECAPTURE MODULE #
 	#***********************#
-	
+
 	## Parameters:
-	
+
 	## Data & constants:
-	
+
 	#----------------------------------#
 	# Likelihood (custom distribution) #
 	#----------------------------------#
-	
+
 	## Define individual sequences of survival and breeding (=recapture) probabilities
 	for(i in 1:n.CH){
 	  for(t in first.sum[i]:last.sum[i]){
@@ -380,52 +380,52 @@ PFC.IPMcode <- nimbleCode({
 	  p.CH[i,first.sum[i]] <- 1
 	}
 	p.ind[1:n.CH,1] <- 1
-	
+
 	## Likelihood with custom distribution
 	for (i in 1:n.CH){
-	  
+
 	  y.sum[i, first.sum[i]:last.sum[i]] ~ dCJS_vv_sum(
 	    probSurvive = phi.CH[i, first.sum[i]:last.sum[i]],
 	    probCapture = p.CH[i, first.sum[i]:last.sum[i]],
 	    len = last.sum[i]-first.sum[i]+1,
 	    mult = CHs.count[i])
 	} #i
-	
-	
+
+
 	#------------------------#
 	# Priors and constraints #
 	#------------------------#
-	
+
 	## Age- and time-dependent survival probabilities
 	phi[1,1:Tmax] <- sJ[1:Tmax]*0.5 # Adjusted for sex ratio
 	phi[2,1:Tmax] <- sA[1:Tmax]
-	
+
 	logit(sJ[1:Tmax]) <- logit(Mu.sJ) + beta3.sJ*cov3[1:Tmax] + epsilon.sJ[1:Tmax]
 	logit(sA[1:Tmax]) <- logit(Mu.sA) + epsilon.sA[1:Tmax]
-	
+
 	Mu.sJ ~ dunif(0, 1)
 	Mu.sA ~ dunif(0, 1)
-	
+
 	## Age- and time-dependent breeding probabilities
-	for (a in 1:A){		
+	for (a in 1:A){
 	  logit(pB[a,1:(Tmax+1)]) <- logit(Mu.pB[a]) + epsilon.pB[1:(Tmax+1)]
 	  Mu.pB[a] ~ dunif(0, 1)
 	}
-	
+
 	## Temporal random effects
 	for(t in 1:Tmax){
 	  epsilon.sJ[t] ~ dnorm(0, sd = sigma.sJ)
 	  epsilon.sA[t] ~ dnorm(0, sd = sigma.sA)
 	}
-	
+
 	for(t in 1:(Tmax+1)){
 	  epsilon.pB[t] ~ dnorm(0, sd = sigma.pB)
 	}
-	
+
 	sigma.sJ ~ dunif(0, 10)
 	sigma.sA ~ dunif(0, 10)
 	sigma.pB ~ dunif(0, 10)
-	                 
+
   ## Covariate effects
 	beta3.sJ ~ dunif(-5, 5)
 
@@ -433,153 +433,153 @@ PFC.IPMcode <- nimbleCode({
 	#********************#
 	# CLUTCH SIZE MODULE #
 	#********************#
-	
+
 	## Parameters:
-	
+
 	## Data & constants:
 
-	
+
   #------------------------------#
 	# Likelihood - age-independent #
 	#------------------------------#
-	
+
 	for(t in 1:Tmax){
-		
+
 		# Expected "true" egg number (by mother age class)
-		EggNo.ex[1:A,t] <- B[1:A,t]*CS[1:A,t] 
-		
+		EggNo.ex[1:A,t] <- B[1:A,t]*CS[1:A,t]
+
 		# Observed egg number (corrected by data availaility)
 		EggNoTot[t] ~ dpois(sum(EggNo.ex[1:A,t])*EggNoSP[t])
-		
+
 	}
-    
+
 	#------------------------------#
 	# Likelihood - age-dependent   #
 	#------------------------------#
-	
+
 	for(x in 1:CS_X){
 		ClutchSize[x] ~ dpois(CS[CS_FAge[x], CS_year[x]])
 	}
 
-	# NOTES: 
-	# The age-dependent likelihood can only be used when we know the age of 
+	# NOTES:
+	# The age-dependent likelihood can only be used when we know the age of
 	# the mother of each brood z in ClutchSize
-	
+
 	#------------------------#
 	# Priors and constraints #
 	#------------------------#
 
-	## Age-dependent clutch size   
-    for(a in 1:A){	
+	## Age-dependent clutch size
+    for(a in 1:A){
     	log(CS[a,1:Tmax]) <- log(Mu.CS[a]) + epsilon.CS[1:Tmax]
     	Mu.CS[a] ~ dunif(0, 10)
     }
-    
+
     ## Temporal random effects
     for(t in 1:Tmax){
     	epsilon.CS[t] ~ dnorm(0, sd = sigma.CS)
     }
-    
+
     sigma.CS ~ dunif(0, 10)
-    
-    
+
+
 	#*************************#
 	# FLEDGING SUCCESS MODULE #
 	#*************************#
-	
+
 	## Parameters:
-	
+
 	## Data & constants:
 
-	
+
   #------------------------------#
 	# Likelihood - age-independent #
 	#------------------------------#
-	
+
 	for(t in 1:Tmax){
-		
+
 		# Expected "true" fledgling number (by mother age class)
-		Fledged.ex[1:A,t] <- EggNo.ex[1:A,t]*pNS[t]*sN[1:A,t] 
-		
+		Fledged.ex[1:A,t] <- EggNo.ex[1:A,t]*pNS[t]*sN[1:A,t]
+
 		# Observed egg number (corrected by data availaility)
 		FledgedTot[t] ~ dpois(sum(Fledged.ex[1:A,t])*FledgedSP[t])
-		
+
 		# Alternative:
 		#FledgedTot[t] ~ dbin(sN[t], LaidTot[t])
-		
+
 	}
-	
+
 	# NOTES:
 	# The alternative likelihood is subsantially simpler, but only possible if we assume
 	# survival to fledging to be independent of mother age
-    
-    
+
+
 	#------------------------------#
 	# Likelihood - age-dependent #
 	#------------------------------#
-	
+
   for(x in 1:F_X){
     anyFledged[x] ~ dbern(pNS[F_year[x]])
   }
-    
+
 	for(x in 1:NoF_X){
 		NoFledged[x] ~ dbin(sN[NoF_FAge[x], NoF_year[x]], NoLaid[x])
 	}
 
-	# NOTES: 
-	# The age-dependent likelihood can only be used when we know the age of 
+	# NOTES:
+	# The age-dependent likelihood can only be used when we know the age of
 	# the mother of each brood z in Fledged/Laid
-	
+
 	#------------------------#
 	# Priors and constraints #
 	#------------------------#
-    
-	## Time- and mother age-dependent fledgling survival  
-    for(a in 1:A){	
+
+	## Time- and mother age-dependent fledgling survival
+    for(a in 1:A){
     	logit(sN[a,1:Tmax]) <- logit(Mu.sN[a]) + beta1.sN*cov1[1:Tmax] + beta2.sN*cov2[1:Tmax] + epsilon.sN[1:Tmax]
     	Mu.sN[a] ~ dunif(0, 1)
     }
-    
+
     logit(pNS[1:Tmax]) <- logit(Mu.pNS) + beta1.pNS*cov1[1:Tmax] + beta2.pNS*cov2[1:Tmax] + epsilon.pNS[1:Tmax]
     Mu.pNS ~ dunif(0, 1)
-    
-    ## Time-dependent fledgling survival  
+
+    ## Time-dependent fledgling survival
     #logit(sN[1:Tmax]) <- logit(Mu.sN) + epsilon.sN[1:Tmax]
     #Mu.sN ~ dunif(0, 1)
 
-    
+
     ## Temporal random effects
     for(t in 1:Tmax){
     	epsilon.sN[t] ~ dnorm(0, sd = sigma.sN)
-      epsilon.pNS[t] ~ dnorm(0, sd = sigma.pNS) 
+      epsilon.pNS[t] ~ dnorm(0, sd = sigma.pNS)
     }
-    
+
     sigma.sN ~ dunif(0, 10)
     sigma.pNS ~ dunif(0, 10)
-    
+
     ## Covariate effects
     beta1.sN ~ dunif(-5, 5)
     beta2.sN ~ dunif(-5, 5)
-    
+
     beta1.pNS ~ dunif(-5, 5)
     beta2.pNS ~ dunif(-5, 5)
-    
-    
+
+
     #*****************************#
     # COVARIATE IMPUTATION MODULE #
     #*****************************#
-    
+
     for(t in 1:Tmax){
       cov1[t] ~ dnorm(0, sd = 1)
       cov2[t] ~ dnorm(0, sd = 1)
       cov3[t] ~ dnorm(0, sd = 1)
     }
-    
-    # NOTE: The imputation model assumes that covariates 
+
+    # NOTE: The imputation model assumes that covariates
     #       are a) normally distributed and b) standardized (scaled and centered).
     #       Other distributions of covariates can be accounted for by changing
-    #       this part. 
-    
+    #       this part.
+
 })
 
 
@@ -597,9 +597,9 @@ source('InitSim.R')
 
 ## Setting parameters to monitor
 parameters <- c(
-  'Mu.sJ', 'Mu.sA', 'Mu.pB', 'Mu.CS', 'Mu.sN', 'Mu.pNS', 
-  'sJ', 'sA', 'pB', 'CS', 'sN', 'pNS', 
-  'sigma.sJ', 'sigma.sA', 'sigma.pB', 'sigma.CS', 'sigma.sN', 'sigma.pNS',  
+  'Mu.sJ', 'Mu.sA', 'Mu.pB', 'Mu.CS', 'Mu.sN', 'Mu.pNS',
+  'sJ', 'sA', 'pB', 'CS', 'sN', 'pNS',
+  'sigma.sJ', 'sigma.sA', 'sigma.pB', 'sigma.CS', 'sigma.sN', 'sigma.pNS',
 	'epsilon.sJ', 'epsilon.sA', 'epsilon.pB', 'epsilon.CS', 'epsilon.sN', 'epsilon.pNS',
   'AvgImm', 'sigma.Imm',
 	'N', 'B', 'Imm', 'ImmB', 'Juv',
